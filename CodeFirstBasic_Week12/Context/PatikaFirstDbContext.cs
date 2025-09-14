3169663090252eb7bbbc2adaea30e9d2bf714f5c
user <user@DESKTOP-U6UHBUC>
2025-09-14 11:42:08 +0300

    Proje dosyası ekle.

---
+++ CodeFirstBasic_Week12/Context/PatikaFirstDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CodeFirstBasic_Week12.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstBasic_Week12.Context
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //SQL Server veritabanına bağlanmak için connection string veriyoruz.
+            optionsBuilder.UseSqlServer("server=DESKTOP-U6UHBUC; database=PatikaCodeFirstDb1; Trusted_Connection=true; TrustServerCertificate=true");
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+        //MovieEntity --> "Movies" adında bir tablo oluşturur/veritabanında bu tabloyu kullanır.
+        //GameEntity --> "Games" adında bir tablo oluşturur/veritabanında bu tabloyu kullanır.
+        public DbSet<GameEntity> Games { get; set; }
+        public DbSet<MovieEntity> Movies { get; set; }
+
+
+    }
+}
---
+++ CodeFirstBasic_Week12/Entities/GameEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeFirstBasic_Week12.Entities
+{
+    public class GameEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public decimal Rating { get; set; }
+
+    }
+}
